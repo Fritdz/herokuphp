baedca4870cd5501f0ef06fe1e11929976cae039
Hafidz <20033454@myrp.edu.sg>
2022-07-07 11:29:51 +0800

    doctype

--- web/test.php
+++ web/test.php
@@ -1,8 +1,12 @@
+<!-- page lacks the HTML doctype -->
 <!-- element does not have a [lang] attribute -->
+<!DOCTYPE html>
 <html lang="en">
     <head>
     <!-- add meta name viewport to optiize mobile screen sizes -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Document does not have a meta description -->
+    <meta name="description" content="This is a test website">
     <!-- document doesn't have title element -->
     <title>Heroku Test Site</title>
     </head>
