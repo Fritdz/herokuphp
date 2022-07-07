383ae483c138dd40188e5c75a3160b5b3d7f9a1e
Hafidz <20033454@myrp.edu.sg>
2022-07-07 11:23:54 +0800

    lighthouse changes

--- web/test.php
+++ web/test.php
@@ -1,7 +1,12 @@
-<html>
+<!-- element does not have a [lang] attribute -->
+<html lang="en">
     <head>
+    <!-- add meta name viewport to optiize mobile screen sizes -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- document doesn't have title element -->
+    <title>Heroku Test Site</title>
+    </head>
         <body>
             Hello World!
         </body>
-    </head>
 </html>
